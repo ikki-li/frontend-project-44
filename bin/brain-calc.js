71d4fb01e1375c6c4e694ac791f4dae8a8ccef67
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-12-10 15:54:00 +0300

    optimize brain-calc game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { calculated } from '../src/games/calc.js';
+import { calculate } from '../src/games/calc.js';
 
-calculated();
+calculate();
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,34 +1,32 @@
 import { cons } from '@hexlet/pairs';
 import { getInteractive } from '../index.js';
 
-// eslint-disable-next-line import/prefer-default-export
-export const calculated = () => {
-  const rulesOfGame = 'What is the result of the expression?';
+const getOneRound = () => {
   const operators = ['+', '*'];
-  let expression = '';
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  const randomOperator = operators[randomIndex];
+  const randomNmb1 = Math.floor(Math.random() * 99 + 1);
+  const randomNmb2 = Math.floor(Math.random() * 9 + 1);
+
+  const expression = `${randomNmb1} ${randomOperator} ${randomNmb2}`;
+
   let rightAnswer = '';
-  const pairs = [];
-
-  for (let i = 1; i < 4; i += 1) {
-    const randomIndex = Math.floor(Math.random() * operators.length);
-    const randomOperator = operators[randomIndex];
-    const randomNmb1 = Math.floor(Math.random() * 99 + 1);
-    const randomNmb2 = Math.floor(Math.random() * 9 + 1);
-
-    expression = `${randomNmb1} ${randomOperator} ${randomNmb2}`;
-
-    switch (randomOperator) {
-      case '+':
-        rightAnswer = String(randomNmb1 + randomNmb2);
-        break;
-      case '*':
-        rightAnswer = String(randomNmb1 * randomNmb2);
-        break;
-      default:
-        rightAnswer = null;
-    }
-    const pair = cons(expression, rightAnswer);
-    pairs.push(pair);
+  switch (randomOperator) {
+    case '+':
+      rightAnswer = String(randomNmb1 + randomNmb2);
+      break;
+    case '*':
+      rightAnswer = String(randomNmb1 * randomNmb2);
+      break;
+    default:
+      rightAnswer = null;
   }
-  getInteractive(rulesOfGame, pairs);
+
+  return cons(expression, rightAnswer);
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const calculate = () => {
+  const rulesOfGame = 'What is the result of the expression?';
+  getInteractive(rulesOfGame, getOneRound);
 };
