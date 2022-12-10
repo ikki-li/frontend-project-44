ca471aeb3a7c95940c6a185a25f8f557c1cee1cb
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-12-10 15:59:06 +0300

    optimize common logic

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import readlineSync from 'readline-sync';
 import { car as expression, cdr as rightAnswer } from '@hexlet/pairs';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getInteractive = (rulesOfGame, pairs) => {
+export const getInteractive = (rulesOfGame, getOneRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rulesOfGame);
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const pair of pairs) {
+  for (let i = 1; i < 4; i += 1) {
+    const pair = getOneRound();
     const answer = readlineSync.question(`Question: ${expression(pair)} `);
     if (answer === rightAnswer(pair)) {
       console.log('Correct!');
     } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer(pair)}".
-Let's try again, ${userName}!`);
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer(pair)}". Let's try again, ${userName}!`);
       return 'error';
     }
   }
