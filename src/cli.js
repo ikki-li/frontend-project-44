2e285c7379be86d1e7a281d4791a23b0c362e475
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-12-13 20:17:44 +0300

    rename function

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getGreeting = () => {
+const greet = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+export default greet;
