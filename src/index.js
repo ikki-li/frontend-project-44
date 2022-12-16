234ea1220042d7de044dd159c73a97e1a818d980
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-12-16 13:32:38 +0300

    fix if...else statement

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import readlineSync from 'readline-sync';
-import { car as expression, cdr as rightAnswer } from '@hexlet/pairs';
+import { car as getQuestion, cdr as getRightAnswer } from '@hexlet/pairs';
 
-const run = (rulesOfGame, getQA) => {
+const roundsCount = 3;
+
+const run = (description, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(rulesOfGame);
+  console.log(description);
 
-  for (let i = 1; i < 4; i += 1) {
-    const pair = getQA();
-    const answer = readlineSync.question(`Question: ${expression(pair)} `);
-    if (answer === rightAnswer(pair)) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer(pair)}". Let's try again, ${userName}!`);
-      return 'error';
+  for (let i = 0; i < roundsCount; i += 1) {
+    const pair = generateRound();
+    const question = getQuestion(pair);
+    const rightAnswer = getRightAnswer(pair);
+    const userAnswer = readlineSync.question(`Question: ${question} `);
+    if (userAnswer !== rightAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
-  return 'passed';
 };
 
 export default run;
