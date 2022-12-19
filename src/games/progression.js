6fc50ff3f62efee3d16c39476cd37a29a8fcb5a8
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-12-19 15:03:39 +0300

    rename variables and imported function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,40 +1,36 @@
-import { cons } from '@hexlet/pairs';
+import { cons as generatePair } from '@hexlet/pairs';
 import run from '../index.js';
 import { getRandomIndex, getRandomNumber } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
-
-// избавилась от магических чисел
 const minLength = 5;
 const maxLength = 10;
-const minNumber = 1;
-const maxNumber = 10;
+const minStep = 2;
+const maxStep = 10;
+const minStart = 1;
+const maxStart = 20;
 
-const createProgression = (progressionLength, firstTerm, step) => {
+const createProgression = (start, step, length) => {
   const progression = [];
-  let term = firstTerm;
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression[i] = term;
-    term += step;
+  let number = start;
+  for (let i = 0; i < length; i += 1) {
+    progression[i] = number;
+    number += step;
   }
   return progression;
 };
 
 const generateRound = () => {
-  const progressionLength = getRandomNumber(minLength, maxLength);
-  const firstTerm = getRandomNumber(minNumber, maxNumber);
-  const step = getRandomNumber(minNumber, maxNumber);
-
-  const progression = createProgression(progressionLength, firstTerm, step);
-
-  const randomIndex = getRandomIndex(progression);
-  const rightAnswer = String(progression[randomIndex]);
-  progression[randomIndex] = '..';
-
+  const length = getRandomNumber(minLength, maxLength);
+  const step = getRandomNumber(minStep, maxStep);
+  const start = getRandomNumber(minStart, maxStart);
+  const progression = createProgression(start, step, length);
+  const index = getRandomIndex(progression);
+  const rightAnswer = String(progression[index]);
+  progression[index] = '..';
   const separator = ' ';
   const question = progression.join(separator);
-
-  return cons(question, rightAnswer);
+  return generatePair(question, rightAnswer);
 };
 
 const runProgression = () => {
